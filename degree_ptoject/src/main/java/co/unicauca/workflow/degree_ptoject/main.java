5aa50a665f052e5940b9f3954543c062b72ec4c8
MaryuriFernandez <maryurifernandez437@gmail.com>
2025-09-02 22:34:39 -0500

    💄Interfaces para los usuarios Docente y Estudiante

--- degree_ptoject/src/main/java/co/unicauca/workflow/degree_ptoject/main.java
+++ degree_ptoject/src/main/java/co/unicauca/workflow/degree_ptoject/main.java
@@ -1,5 +1,6 @@
 package co.unicauca.workflow.degree_ptoject;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,14 +8,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class main extends Application {
+    
+    private static Scene scene;
+    
   @Override
   public void start(Stage stage) throws Exception {
-    Parent root = FXMLLoader.load(
-        getClass().getResource("/co/unicauca/workflow/degree_ptoject/view/login.fxml")
-    );
-    stage.setTitle("Login");
-    stage.setScene(new Scene(root));
-    stage.show();
+      scene = new Scene(loadFXML("login"));
+      stage.setScene(scene);
+      stage.show();
+  }
+  
+  public static void setRoot(String fxml) throws IOException {
+      scene.setRoot(loadFXML(fxml));
+  }
+  
+  private static Parent loadFXML(String fxml) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("/co/unicauca/workflow/degree_ptoject/view/" + fxml + ".fxml"));
+    return fxmlLoader.load();
   }
 
   public static void main(String[] args) {

6fe9318bbbf9619e67c29625f989a46af164679d
Juan Ortega <jmo.narvaez@gmail.com>
2025-09-18 21:04:40 -0500

    ✨ feat: agregar modelos de dominio para proyecto y archivos
    incluye entidades básicas (Archivo, Proyecto) y sus enums asociados para representar tipos y estados.

---
+++ degree_project/src/main/java/co/unicauca/workflow/degree_project/domain/models/EstadoArchivo.java
@@ -0,0 +1,8 @@
+package co.unicauca.workflow.degree_project.domain.models;
+
+public enum EstadoArchivo {
+    PENDIENTE,
+    APROBADO,
+    RECHAZADO,
+    OBSERVADO
+}
---
+++ degree_project/src/main/java/co/unicauca/workflow/degree_project/domain/models/EstadoProyecto.java
@@ -0,0 +1,7 @@
+package co.unicauca.workflow.degree_project.domain.models;
+
+public enum EstadoProyecto {
+    EN_TRAMITE,
+    CANCELADO,
+    TERMINADO
+}
